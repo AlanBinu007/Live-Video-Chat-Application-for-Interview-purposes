c231578f7a2421abe0515645dc59258abfa0e192
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-06-19 13:44:08 +0530

    Fixed the live id issue

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import homeIcon from '../../assets/video-call.png';
-import { APP_NAME, repoName } from '../../constants';
 import './Navbar.css';
 import StarIcon from '@material-ui/icons/Star';
 
@@ -9,9 +8,9 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='title-div'>
         <img src={homeIcon} alt='' />
-        <h3>{APP_NAME}</h3>
+        <h3>HAVE FUN</h3>
       </div>
-      <a className='repo' href="" target='_blank'>
+      <a className='repo' href="https://github.com/AlanBinu007/Online-Live-Video-Chat-Application-for-Interview-purposes" target='_blank'>
         <StarIcon className='rotate' />
         <div className='repo-text'>
           <p className='github-name'>Github</p>
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,4 @@
 export const APP_NAME = 'Merge';
-export const repoName = 'https://github.com/suresh213/video-chat';
-export const APP_URL = 'https://merge.netlify.app';
+export const APP_URL = 'https://alanbinu-video-conference.web.app';
 export const BACKEND_URL = 'https://video-chat-meet-backend.herokuapp.com';
 // export const BACKEND_URL='http://localhost:5000';
